2062c089dfa2c9e69ed194299bd74e0d632fd37e
shtabin98 <shtabin98@gmail.com>
2026-02-13 22:03:13 +0700

    Задание 1 и 2

---
+++ Source2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "Введите число:\n";
+	cin >> num;
+	cout << "Вы ввели:\n" << num << endl;
+
+	string str;
+
+	cout << "Введите слово:\n";
+	cin >> str;
+	cout << "Вы ввели:\n" << str << endl;
+
+	return 0;
+}
