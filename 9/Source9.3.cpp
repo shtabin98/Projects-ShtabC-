f902d99eb2fdd8b04d8a724e37187fb7aa219d71
shtabin98 <shtabin98@gmail.com>
2026-03-06 15:55:21 +0700

    Задание 9.3

---
+++ 9/Source9.3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+void print(int* mas, int size);
+void reverse(int* mas, int size);
+
+int main()
+{
+	int mas[]{ 1,2,3,4,5,6,7,8,9 };
+	int size = sizeof(mas) / 4;
+	
+	std::cout << "До функции reverse: ";
+	print(mas, size);
+
+	reverse(mas, size);
+
+	std::cout << "После функции reverse: ";
+	print(mas, size);
+
+	return 0;
+}
+
+
+void print(int* mas, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << mas[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+void reverse(int* mas, int size)
+{
+	for (int i = 0; i < size/2; ++i)
+	{
+		int vremes = mas[i];
+		mas[i] = mas[(size - 1) - i];
+		mas[(size - 1) - i] = vremes;
+	}
+}
