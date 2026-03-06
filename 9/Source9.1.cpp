4b9b680df1aae4d1f96b6ae1fda215bbee9b64b7
shtabin98 <shtabin98@gmail.com>
2026-03-06 15:54:14 +0700

    Задание 9.1

---
+++ 9/Source9.1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void print(int* mas, int size);
+
+int main()
+{
+	int mas_1[]{ 1,2,3,4,5,6,7,8,9 };
+	int mas_2[]{ 6,5,4,8 };
+	int mas_3[]{ 1,4,3,7,5 };
+
+	int size_1 = sizeof(mas_1) / 4;
+	int size_2 = sizeof(mas_2) / 4;
+	int size_3 = sizeof(mas_3) / 4;
+
+	print(mas_1, size_1);
+	print(mas_2, size_2);
+	print(mas_3, size_3);
+
+	return 0;
+}
+
+void print(int* mas, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << mas[i] << " ";
+	}
+	std::cout << "\n";
+}
