d0d60a199b7d8a4559cc65585f4a76d59d522adc
shtabin98 <shtabin98@gmail.com>
2026-05-27 07:43:34 +0300

    Add files via upload

--- Базовое программирование/ООП. Наследование и полиморфизм/Задача 3. Фигуры. Методы/Source.cpp
+++ Базовое программирование/ООП. Наследование и полиморфизм/Задача 3. Фигуры. Методы/Source.cpp
@@ -5,8 +5,9 @@ class figure
 protected:
 	std::string __figura{ "Фигура: " };
 	int __storon{ 0 };
-	int __a{ }, __b{ }, __c{ }, __d{ };
-	int __A{ }, __B{ }, __C{ }, __D{ };
+	int __a{0}, __b{0}, __c{0}, __d{0};
+	int __A{0}, __B{0}, __C{0}, __D{0};
+
 public:
 	figure(std::string figura, int storon, int a, int b, int c, int d, int A, int B, int C, int D)
 	{
@@ -15,16 +16,30 @@ public:
 		__a = a; __b = b; __c = c; __d = d; __A = A; __B = B; __C = C; __D = D;
 	};
 	figure() {};
+
 	std::string get_figura() const { return __figura; };
 	int get_storon() const { return __storon; };
 	int get_a() const { return __a; }; int get_b() const { return __b; }; int get_c() const { return __c; }; int get_d() const { return __d; };
 	int get_A() const { return __A; }; int get_B() const { return __B; }; int get_C() const { return __C; }; int get_D() const { return __D; };
 
-	std::string vshivost() 
+	virtual bool virt_method()
 	{
-		if (get_storon() == 0) { return "Правильная"; }
-		else { return "Неправильная"; }
+		if (get_storon() == 0) { return true; }
+		else { return false; }
 	};
+	
+	virtual std::string check()
+	{
+		std::string check = { "Правильная" };
+		if (virt_method() == true) { check; }
+		else (check = "Неправильная");
+		return check;
+	};
+
+	void print_info()
+	{
+		std::cout << get_figura() << std::endl << check() << std::endl << "Количество сторон: " << get_storon() << std::endl << std::endl;
+	}
 };
 
 class treu : public figure
@@ -35,27 +50,48 @@ public:
 		__figura = "Треугольник";
 	};
 
-	std::string vshivost()
+	bool virt_method()
 	{
 		int plus = __A + __B + __C;
-		if (get_storon() == 3 && plus == 180 ) { return "Правильная"; }
-		else { return "Неправильная"; }
+		if (get_storon() == 3 && plus == 180) { return true; }
+		else { return false; }
 	};
+
+	void print_info()
+	{
+		std::cout << get_figura() << std::endl << check() << std::endl << "Количество сторон: " << get_storon() << std::endl
+			<< "Стороны: a=" << get_a() << " b=" << get_b()
+			<< " c=" << get_c() << std::endl
+			<< "Углы: A=" << get_A() << " B=" << get_B()
+			<< " C=" << get_C() << std::endl << std::endl;
+	}
 };
 
 class chetyre : public figure
 {
 public:
-	chetyre() : figure(" ", 4, 10, 20, 30, 40, 50, 60, 70, 80)
+	chetyre() : figure(" ", 4, 10, 20, 30, 40, 50, 80, 130, 100)
 	{
 		__figura = "Четырехугольник";
 	};
-	std::string vshivost()
+
+	bool virt_method()
 	{
 		int plus = __A + __B + __C + __D;
-		if (get_storon() == 4 && plus == 360) { return "Правильная"; }
-		else { return "Неправильная"; }
+		if (get_storon() == 4 && plus == 360) { return true; }
+		else { return false; }
 	};
+
+	void print_info()
+	{
+		std::cout << get_figura() << std::endl << check() << std::endl
+			<< "Количество сторон: " << get_storon() << std::endl
+			<< "Стороны: a=" << get_a() << " b=" << get_b()
+			<< " c=" << get_c() << " d=" << get_d() << std::endl
+			<< "Углы: A=" << get_A() << " B=" << get_B()
+			<< " C=" << get_C() << " D=" << get_D() << std::endl << std::endl;
+
+	}
 };
 
 class pryamougolnyi : public treu
@@ -63,14 +99,17 @@ class pryamougolnyi : public treu
 public:
 	pryamougolnyi() : treu()
 	{
+		__A = 30;
 		__C = 90;
 		__figura = "Прямоугольный треугольник: ";
 	};
-	std::string vshivost()
+
+	bool virt_method()
 	{
 		int plus = __A + __B + __C;
-		if (get_storon() == 3 && plus == 180 && this-> __C == 90) { return "Правильная"; }
-		else { return "Неправильная"; }
+		if (get_storon() == 3 && plus == 180 && this-> __C == 90)
+		{ return true; }
+		else { return false; }
 	};
 };
 
@@ -79,15 +118,16 @@ class ravnobedr : public treu
 public:
 	ravnobedr() : treu()
 	{
-		__c = __a; __C = __A;
+		__c = __a; __C = __A; __B = 80;
 		__figura = "Равнобедренный треугольник: ";
 	};
-	std::string vshivost()
+
+	bool virt_method()
 	{
 		int plus = __A + __B + __C;
 		if (get_storon() == 3 && plus == 180 && __c == __a && __C == __A)
-		{ return "Правильная"; }
-		else { return "Неправильная"; }
+		{ return true; }
+		else { return false; }
 	};
 };
 
@@ -102,15 +142,14 @@ public:
 		__B = __A; __C = __B;
 		__figura = "Равноcторонний треугольник: ";
 	}
-	std::string vshivost()
+
+	bool virt_method()
 	{
 		int plus = __A + __B + __C;
 		if (get_storon() == 3 && plus == 180 && __b == __a && __c == __b
-			&& __A == 60 && __B ==__A && __C == __B)
-		{
-				return "Правильная";
-		}
-		else { return "Неправильная"; }
+			&& __A == 60 && __B == __A && __C == __B)
+		{return true;}
+		else { return false; };
 	};
 };
 
@@ -124,16 +163,15 @@ public:
 		__A = 90;
 		__B = __A; __C = __B; __D = __C;
 	};
-	std::string vshivost()
+
+	bool virt_method()
 	{
 		int plus = __A + __B + __C + __D;
 		if (get_storon() == 4 && plus == 360 && __a == __c && __d == __b
 			&& __A == 90 && __B == __A && __C == __B && __D == __C)
-		{
-			return "Правильная";
-		}
-		else { return "Неправильная"; }
-	};
+		{return true;}
+		else { return false; };
+	}
 };
 
 class kvadrat : public chetyre
@@ -146,17 +184,16 @@ public:
 		__B = __A; __C = __B; __D = __C;
 		__figura = "Квадрат: ";
 	};
-	std::string vshivost()
+	
+	bool virt_method()
 	{
 		int plus = __A + __B + __C + __D;
 		if (get_storon() == 4 && plus == 360 &&
 			__a == __b && __b == __c && __d == __c
 			&& __A == 90 && __B == __A && __C == __B && __D == __C)
-		{
-			return "Правильная";
-		}
-		else { return "Неправильная"; }
-	};
+		{return true;}
+		else { return false; };
+	}
 };
 
 class parralelogram : public chetyre
@@ -165,19 +202,19 @@ public:
 	parralelogram() : chetyre()
 	{
 		__c = __a; __d = __b;
+		__A = 80; __B = 100;
 		__C = __A; __D = __B;
 		__figura = "Параллелограмм: ";
 	};
-	std::string vshivost()
+	
+	bool virt_method()
 	{
 		int plus = __A + __B + __C + __D;
 		if (get_storon() == 4 && plus == 360 && __a == __c && __d == __b
 			&& __C == __A && __B == __D)
-		{
-			return "Правильная";
-		}
-		else { return "Неправильная"; }
-	};
+		{return true;}
+		else { return false; };
+	}
 };
 
 class romb : public chetyre
@@ -186,77 +223,53 @@ public:
 	romb() : chetyre()
 	{
 		__b = __a; __c = __b; __d = __c;
+		__A = 100;
 		__C = __A; __D = __B;
 		__figura = "Ромб: ";
 	};
-	std::string vshivost()
+
+	bool virt_method()
 	{
 		int plus = __A + __B + __C + __D;
 		if (get_storon() == 4 && plus == 360 &&
 			__a == __b && __c == __b && __c == __d
 			&& __C == __A && __D == __B)
-		{
-			return "Правильная";
-		}
-		else { return "Неправильная"; }
+		{return true;}
+		else { return false;}
 	};
 };
-
-void print_info(figure& figa)
-{
-	figure f;
-	figure* gu{ &f };
-	switch (figa.get_storon())
-	{
-	case 0:
-		std::cout << figa.get_figura() << std::endl << gu->vshivost() << "\nКоличество сторон: " << figa.get_storon() << std::endl << std::endl;
-		break;
-	case 3:
-		std::cout << figa.get_figura() << std::endl << gu->vshivost() << "\nКоличество сторон: " << figa.get_storon() << std::endl << "Стороны: a=" << figa.get_a() << " b=" << figa.get_b()
-			<< " c=" << figa.get_c() << std::endl
-			<< "Углы: A=" << figa.get_A() << " B=" << figa.get_B()
-			<< " C=" << figa.get_C() << std::endl << std::endl;
-		break;
-	case 4:
-		std::cout << figa.get_figura() << std::endl << gu->vshivost() << "\nКоличество сторон: " << figa.get_storon() << std::endl << "Стороны: a=" << figa.get_a() << " b=" << figa.get_b()
-			<< " c=" << figa.get_c() << " d=" << figa.get_d() << std::endl
-			<< "Углы: A=" << figa.get_A() << " B=" << figa.get_B()
-			<< " C=" << figa.get_C() << " D=" << figa.get_D() << std::endl << std::endl;
-		break;
-	}
-}
 
 int main()
 {
 	figure fi;
-	print_info(fi);
+	fi.print_info();
 
 	treu tre;
-	print_info(tre);
+	tre.print_info();
 
 	pryamougolnyi tr;
-	print_info(tr);
+	tr.print_info();
 
 	ravnobedr trz;
-	print_info(trz);
+	trz.print_info();
 
 	ravnostor tro;
-	print_info(tro);
+	tro.print_info();
 
 	chetyre chet;
-	print_info(chet);
+	chet.print_info();
 
 	prymougol pr;
-	print_info(pr);
+	pr.print_info();
 
 	kvadrat kv;
-	print_info(kv);
+	kv.print_info();
 
 	parralelogram par;
-	print_info(par);
+	par.print_info();
 
 	romb romb;
-	print_info(romb);
+	romb.print_info();
 	
 	return 0;
 }
