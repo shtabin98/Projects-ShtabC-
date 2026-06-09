aee25f0081b2e1c8c130c5fc004f8b0088956d5b
shtabin98 <shtabin98@gmail.com>
2026-06-10 02:32:24 +0300

    Update Source

--- Базовое программирование/Препроцессор и макросы/Задание 1. Препроцессорные директивы/Source.cpp
+++ Базовое программирование/Препроцессор и макросы/Задание 1. Препроцессорные директивы/Source.cpp
@@ -6,13 +6,17 @@
 	#error "Необходимо определить MODE"
 #endif
 
+int add(int i, int j)
+{
+	return i + j;
+}
+
 int main()
 {
 #if MODE == 0
 	std::cout << "Работаю в режиме тренировки";
 #elif MODE == 1
 	int i, j;
-	int add(int i, int j);
 	std::cout << "Работаю в боевом режиме\n";
 	std::cout << "Введите число 1: "; std::cin >> i;
 	std::cout << "Введите число 2: "; std::cin >> j;
@@ -21,9 +25,4 @@ int main()
 	std::cout << "Неизвестный режим.Завершение работы";
 #endif
 		return 0;
-}
-
-int add(int i, int j)
-{
-	return i + j;
 }
