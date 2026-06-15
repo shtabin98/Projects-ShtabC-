6b850be5b6b44d9c5ac7b3ebbc5deb74dcbf03d1
shtabin98 <shtabin98@gmail.com>
2026-06-15 10:48:15 +0300

    Update Source.cpp

--- Базовое программирование/Препроцессор и макросы/Задание 1. Препроцессорные директивы/Source.cpp
+++ Базовое программирование/Препроцессор и макросы/Задание 1. Препроцессорные директивы/Source.cpp
@@ -17,6 +17,10 @@ int main()
 	std::cout << "Работаю в режиме тренировки";
 #elif MODE == 1
 	int i, j;
+	int add(int i, int j)
+	{
+		return i + j;
+	};
 	std::cout << "Работаю в боевом режиме\n";
 	std::cout << "Введите число 1: "; std::cin >> i;
 	std::cout << "Введите число 2: "; std::cin >> j;
@@ -24,5 +28,5 @@ int main()
 #else
 	std::cout << "Неизвестный режим.Завершение работы";
 #endif
-		return 0;
+	return 0;
 }
