50475d68fc0ccd318c35be48562c526e3d89017d
shtabin98 <shtabin98@gmail.com>
2026-06-17 11:30:32 +0300

    Add files via upload

---
+++ Базовое программирование/Тестирование и Unit-тесты/Задание 2/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\MiAir\source\repos\Базовое программирование\Unit - тестирование\Задание 2\Project1\extended_array.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(ExtendedArrayTest)
+	{
+	public:
+		
+		TEST_METHOD(Mean)
+		{
+			ExtArray<int> arr({ 1,5,5,5,5 });
+			Assert::AreEqual(5.0, arr.mean());
+		}
+		TEST_METHOD(Mean_beg_end)
+		{
+			ExtArray<int> arr({ 1,4,8,1,1 });
+			Assert::AreEqual(5.0, arr.mean(2,3));
+		}
+	};
+}
