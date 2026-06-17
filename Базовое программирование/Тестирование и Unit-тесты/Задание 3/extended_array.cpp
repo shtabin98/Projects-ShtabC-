0a7f39bc854cd31762cc69841e40179024df6684
shtabin98 <shtabin98@gmail.com>
2026-06-17 11:36:38 +0300

    Add files via upload

---
+++ Базовое программирование/Тестирование и Unit-тесты/Задание 3/extended_array.cpp
@@ -0,0 +1,11 @@
+#include "extended_array.h"
+
+int main()
+{
+	ExtArray ex({ 1,2,0,0,0,3,1,1 });
+	std::cout << ex.mean() << std::endl;
+	std::cout << ex.mean(4, 6) << std::endl;
+	std::cout << ex.median() << std::endl;
+	std::cout << ex.checkSum();
+	//ex.mode();
+}
