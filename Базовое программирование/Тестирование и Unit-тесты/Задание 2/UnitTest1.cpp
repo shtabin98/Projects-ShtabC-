d6a054636bcd194898ff9c1bfb9a744b3fe02647
shtabin98 <shtabin98@gmail.com>
2026-06-23 02:47:28 +0300

    Add files via upload

--- Базовое программирование/Тестирование и Unit-тесты/Задание 2/UnitTest1.cpp
+++ Базовое программирование/Тестирование и Unit-тесты/Задание 2/UnitTest1.cpp
@@ -9,16 +9,32 @@ namespace UnitTest1
 	TEST_CLASS(ExtendedArrayTest)
 	{
 	public:
-		
-		TEST_METHOD(Mean)
+		//Тесты для корректного 
+
+		TEST_METHOD(Mean) //Среднее для диапазона
 		{
-			ExtArray<int> arr({ 1,5,5,5,5 });
-			Assert::AreEqual(5.0, arr.mean());
+			ExtArray<int> arr({ 2,3,4 });
+			Assert::AreEqual(2.5, arr.mean(0, 2));
 		}
-		TEST_METHOD(Mean_beg_end)
+
+		TEST_METHOD(Mean_all) //Среднее для всего массива
+		{
+			ExtArray<int> arr({ 2,3,4 });
+			Assert::AreEqual(3.0, arr.mean());
+		}
+
+		//Тесты для некорректного 
+
+		TEST_METHOD(Mean_beg_end) //Для диапазона 
 		{
 			ExtArray<int> arr({ 1,4,8,1,1 });
-			Assert::AreEqual(5.0, arr.mean(2,3));
+			Assert::AreEqual(5.0, arr.mean(2, 3));
+		}
+
+		TEST_METHOD(Mean_beg_end_all) //Для всего массива
+		{
+			ExtArray<int> arr({ 2,3,4 });
+			Assert::AreEqual(8.0, arr.mean());
 		}
 	};
 }
